d20e889ee83af17960ec615273418b40a370a8e0
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-02-26 14:38:56 +0100

    Add project files.

---
+++ Tomi_Nezoter_2021.02.26/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tomi_Nezoter_2021._02._26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
